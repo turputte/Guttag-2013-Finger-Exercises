d87d4a1819bdcf63d42405ac30769d930e6aa5b9
turputte <83078233+turputte@users.noreply.github.com>
2021-04-23 19:26:04 +0200

    Create finger_exercise_2.2_v2.py

---
+++ finger_exercise_2.2_v2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# John Guttag - Introduction to computation and programming
+# using python
+# Chapter 2.2 Finger Exercise
+# Finger exercise: Write a program that examines three variables— x , y , and z —
+# and prints the largest odd number among them. If none of them are odd, it
+# should print a message to that effect.
+while True:
+    try:
+        x = int(input('Write an integer x='))
+        break
+    except ValueError:
+        print('Only integers')
+while True:
+    try:
+        y = int(input('Write an integer y='))
+        break
+    except ValueError:
+        print('Only integers')
+while True:
+    try:
+        z = int(input('Write an integer z='))
+        break
+    except ValueError:
+        print('Only integers')
+
+nums = [x, y, z]
+odds = [i for i in nums if i % 2 != 0]
+if odds:
+    print(max(odds))
+else:
+    print('None of the numbers are odd!')
