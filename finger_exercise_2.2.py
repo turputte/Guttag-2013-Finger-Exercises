8aef41886cd136a3bddc11b1e687f569e974cf3b
turputte <83078233+turputte@users.noreply.github.com>
2021-04-23 18:34:15 +0200

    Update finger_exercise_2.2.py

--- finger_exercise_2.2.py
+++ finger_exercise_2.2.py
@@ -5,10 +5,50 @@
 # Finger exercise: Write a program that examines three variables— x , y , and z —
 # and prints the largest odd number among them. If none of them are odd, it
 # should print a message to that effect.
-
-numbers = [input('Enter a value: ') for i in range(10)]
-odds = [y for y in numbers if y % 2 != 0]
-if odds:
-	print(max(odds))
-else:
-	print('All declared variables have even values.')
+while True:
+    try:
+        x = int(input('Write an integer x='))
+        break
+    except ValueError:
+        print('Only integers')
+while True:
+    try:
+        y = int(input('Write an integer y='))
+        break
+    except ValueError:
+        print('Only integers')
+while True:
+    try:
+        z = int(input('Write an integer z='))
+        break
+    except ValueError:
+        print('Only integers')
+if x % 2 == 0 and y % 2 == 0 and z % 2 == 0:
+    print('None are odd numbers!')
+elif x % 2 == 0 and y % 2 == 0:
+    print('z is the only and largest odd number')
+elif y % 2 == 0 and z % 2 == 0:
+    print('x is the only and largest odd number')
+elif x % 2 == 0 and z % 2 == 0:
+    print('y is the only and largest odd number')
+elif x % 2 == 0:
+    if y > z:
+        print('y is the largest odd number')
+    elif y == z:
+        print('y and z are equal and are the largest odd numbers')
+    else:
+        print('z is the largest odd number')
+elif y % 2 == 0:
+    if x > z:
+        print('x is the largest odd number')
+    elif x == z:
+        print('x and z are equal and are the largest odd numbers')
+    else:
+        print('z is the largest odd number')
+elif z % 2 == 0:
+    if x > y:
+        print('x is the largest odd number')
+    elif x == y:
+        print('x and y are equal and are the largest odd numbers')
+    else:
+        print('z is the largest odd number')
