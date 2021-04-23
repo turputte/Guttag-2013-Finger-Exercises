a44da0e8de83fbf088809f528b4e982d53ef2072
turputte <83078233+turputte@users.noreply.github.com>
2021-04-23 18:50:28 +0200

    Update finger_exercise_2.2.py

--- finger_exercise_2.2.py
+++ finger_exercise_2.2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # John Guttag - Introduction to computation and programming
 # using python
 # Chapter 2.2 Finger Exercise
@@ -24,31 +25,32 @@ while True:
     except ValueError:
         print('Only integers')
 if x % 2 == 0 and y % 2 == 0 and z % 2 == 0:
-    print('None are odd numbers!')
+    print('Non are odd numbers')
 elif x % 2 == 0 and y % 2 == 0:
-    print('z is the only and largest odd number')
+    print(z, ' is the only and largest odd number')
 elif y % 2 == 0 and z % 2 == 0:
-    print('x is the only and largest odd number')
+    print(x, ' is the only and largest odd number')
 elif x % 2 == 0 and z % 2 == 0:
-    print('y is the only and largest odd number')
+    print(y, ' is the only and largest odd number')
 elif x % 2 == 0:
     if y > z:
-        print('y is the largest odd number')
+        print(y, ' is the largest odd number')
     elif y == z:
-        print('y and z are equal and are the largest odd numbers')
+        print(y, z, ' are equal and are the largest odd numbers')
     else:
-        print('z is the largest odd number')
+        print(z, ' is the largest odd number')
 elif y % 2 == 0:
     if x > z:
-        print('x is the largest odd number')
+        print(x, ' is the largest odd number')
     elif x == z:
-        print('x and z are equal and are the largest odd numbers')
+        print(x, z, ' are equal and are the largest odd numbers')
     else:
-        print('z is the largest odd number')
+        print(z, ' is the largest odd number')
 elif z % 2 == 0:
     if x > y:
-        print('x is the largest odd number')
+        print(x, ' is the largest odd number')
     elif x == y:
-        print('x and y are equal and are the largest odd numbers')
+        print(x, y, ' are equal and are the largest odd numbers')
     else:
-        print('z is the largest odd number')
+        print(z, ' is the largest odd number')
+
